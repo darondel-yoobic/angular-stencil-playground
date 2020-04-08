f8057c6b3eabe376b7ccd507225cdae63c6a4289
darondel-yoobic <darondel@yoobic.com>
2020-04-08 11:32:53 +0100

    Link the Stencil workspace to the Angular one

--- angular/src/main.ts
+++ angular/src/main.ts
@@ -4,9 +4,15 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
+import { applyPolyfills, defineCustomElements } from 'stencil/loader';
+
 if (environment.production) {
   enableProdMode();
 }
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
+
+applyPolyfills().then(() => {
+  defineCustomElements();
+});
